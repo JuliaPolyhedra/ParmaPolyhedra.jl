7cf3090005e75c600be20b3bf53d67f5f74be4f0
Benoît Legat <benoit.legat@gmail.com>
2023-01-17 12:34:26 +0100

    Working building of ConstraintSystem

--- src/ParmaPolyhedra.jl
+++ src/ParmaPolyhedra.jl
@@ -2,4 +2,11 @@ module ParmaPolyhedra
 
 include("gen/ppl.jl")
 
+function __init__()
+    err = PPL.ppl_initialize()
+    if !iszero(err)
+        error("Error in initialization of the Parma Polyhedra Library: `ppl_initialize` returned error code `$err`.")
+    end
+end
+
 end # module ParmaPolyhedra
