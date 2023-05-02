6874f7b88acc7e34353b3e96c1244d5b2cabc1b7
Benoît Legat <benoit.legat@gmail.com>
2023-05-02 09:44:31 +0200

    Add conversion to H-rep

--- src/hrep.jl
+++ src/hrep.jl
@@ -2,23 +2,16 @@ struct ConstraintSystem <: Polyhedra.MixedHRep{Rational{BigInt}}
     data::PPL.ppl_Constraint_System_t
 end
 
-function Polyhedra.FullDim(cs::ConstraintSystem)
+function Polyhedra.FullDim(cs::ConstraintSystem)::Int # ppl returns `UInt64` but Polyhedra expects `Int`
     return ppl_get(PPL.ppl_dimension_type, PPL.ppl_Constraint_System_space_dimension, cs.data)
 end
 
 Polyhedra.hvectortype(::Type{ConstraintSystem}) = Vector{Rational{BigInt}}
 
-struct ConstraintSystemIndex{T,ElemT}
+struct ConstraintSystemIndex{T,ElemT} <: Polyhedra.AbstractIndex{T,ElemT}
     iterator::PPL.ppl_Constraint_System_const_iterator_t
 end
 # FIXME add finalizer to free iterator
-
-# TODO We could get rid of these ones if Polyhedra had an `AbstractIndex`
-Polyhedra.undouble_it(idx) = idx[1]
-Polyhedra.double_it(idx::ConstraintSystemIndex) = idx, idx
-function Base.iterate(idxs::Polyhedra.Indices, idx::ConstraintSystemIndex)
-    return Polyhedra.double_it(Polyhedra.nextindex(idxs.rep, idx))
-end
 
 # Different than Polyhedra.mixednext since we do not start by skipping `it`
 function _mixed_next!(cs::ConstraintSystem, idx::ConstraintSystemIndex{T,ElemT}) where {T,ElemT}
@@ -42,7 +35,7 @@ function Polyhedra.nextindex(cs::ConstraintSystem, idx::ConstraintSystemIndex)
     return _mixed_next!(cs, idx)
 end
 
-function Base.isvalid(cs::ConstraintSystem, idx::ConstraintSystemIndex{T,ElemT}) where {T,ElemT}
+function Base.isvalid(::ConstraintSystem, idx::ConstraintSystemIndex{T,ElemT}) where {T,ElemT}
     pc = ppl_get(PPL.ppl_const_Constraint_t, PPL.ppl_Constraint_System_const_iterator_dereference, idx.iterator)
     return (PPL.ppl_Constraint_type(pc) == PPL.PPL_CONSTRAINT_TYPE_EQUAL) == Polyhedra.islin(ElemT)
 end
@@ -52,7 +45,29 @@ function Polyhedra.done(cs::Polyhedra.HIndices, idx::ConstraintSystemIndex)
     return !iszero(PPL.ppl_Constraint_System_const_iterator_equal_test(idx.iterator, it_end))
 end
 
-function add_element!(cs::ConstraintSystem, h::Polyhedra.HalfSpace)
+function Base.get(::ConstraintSystem, idx::ConstraintSystemIndex{T,ElemT}) where {T,ElemT}
+    ct = ppl_get(PPL.ppl_Constraint_t, PPL.ppl_Constraint_System_const_iterator_dereference, idx.iterator)
+    @assert _constraint_type(ElemT) == PPL.ppl_Constraint_type(ct)
+    coef = ppl_new(PPL.ppl_Coefficient_t, PPL.ppl_new_Coefficient)
+    ppl(PPL.ppl_Constraint_inhomogeneous_term, ct, coef)
+    β = zero(BigInt)
+    ppl(PPL.ppl_Coefficient_to_mpz_t, coef, β)
+    n = ppl_get(PPL.ppl_dimension_type, PPL.ppl_Constraint_space_dimension, ct)
+    a = zeros(BigInt, n)
+    for i in eachindex(a)
+        # `i - 1` because PPL is zero-indexed
+        ppl(PPL.ppl_Constraint_coefficient, ct, i - 1, coef)
+        ppl(PPL.ppl_Coefficient_to_mpz_t, coef, a[i])
+    end
+    ppl(PPL.ppl_delete_Coefficient, coef)
+    #ppl(PPL.ppl_delete_Constraint, ct) # FIXME segfaults
+    return ElemT(convert(Vector{T}, a), convert(T, β))
+end
+
+_constraint_type(::Type{<:Polyhedra.HalfSpace}) = PPL.PPL_CONSTRAINT_TYPE_GREATER_OR_EQUAL
+_constraint_type(::Type{<:Polyhedra.HyperPlane}) = PPL.PPL_CONSTRAINT_TYPE_EQUAL
+
+function add_element!(cs::ConstraintSystem, h::Polyhedra.HRepElement)
     c = ppl_new(PPL.ppl_Coefficient_t, PPL.ppl_new_Coefficient)
     le = ppl_new(PPL.ppl_Linear_Expression_t, PPL.ppl_new_Linear_Expression)
     for i in 1:Polyhedra.fulldim(h)
@@ -61,7 +76,7 @@ function add_element!(cs::ConstraintSystem, h::Polyhedra.HalfSpace)
     end
     ppl(PPL.ppl_assign_Coefficient_from_mpz_t, c, convert(BigInt, h.β))
     ppl(PPL.ppl_Linear_Expression_add_to_inhomogeneous, le, c)
-    ct = ppl_new(PPL.ppl_Constraint_t, PPL.ppl_new_Constraint, le, PPL.PPL_CONSTRAINT_TYPE_GREATER_OR_EQUAL)
+    ct = ppl_new(PPL.ppl_Constraint_t, PPL.ppl_new_Constraint, le, _constraint_type(typeof(h)))
     ppl(PPL.ppl_Constraint_System_insert_Constraint, cs.data, ct)
     ppl(PPL.ppl_delete_Constraint, ct)
     ppl(PPL.ppl_delete_Linear_Expression, le)
